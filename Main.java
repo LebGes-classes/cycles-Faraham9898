b8cf8982abf8eeaac1074ee35d97d9d3199420d3
Faraham98 <kirya.lapshov.90@gmail.com>
2023-10-07 01:18:09 +0300

    ДЗ 07.10

--- Main.java
+++ Main.java
@@ -1,31 +1,12 @@
-import java.lang.Math;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int x = -9;
-        while (x >= -9 && x <= 9) {
-            double g = chetY(x);
-            System.out.println(g);
-            x++;
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+        String res = "";
+        for (int i = 0; i < string.length(); i++) {
+            res = string.charAt(i) + res;
         }
-    }
-    public static double chetY(int x){
-        double y = 0;
-        if (x <= -6) {
-            y = -Math.sqrt(9 - Math.pow((x + 6), 2));
-        }
-        if (x > -6 && x <= -3) {
-            y = x + 3;
-        }
-        if (x > -3 && x <= 0) {
-            y = Math.sqrt(9 - Math.pow((x), 2));
-        }
-        if (x > 0 && x <= 3) {
-            y = -x + 3;
-        }
-        if (x > 3 && x <= 9){
-            y = x * 0.5 - 3 * 0.5;
-        }
-        return y;
+        System.out.println(res);
     }
 }
